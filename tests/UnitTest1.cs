c77f191bfb048dd5878108050fff0b9302a5f749
den9995 <den9995@gmail.com>
2023-04-16 22:08:41 +0300

    tests

--- tests/UnitTest1.cs
+++ tests/UnitTest1.cs
@@ -1,46 +1,50 @@
-namespace tests;
 using cli_life;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-public class UnitTest1
+namespace Tests
 {
-    [Fact]
-    public void Test1()
+    [TestClass]
+    public class UnitTest1
     {
-        Program.Reset();
-        Board result = Program.board;
-        Board expected = new Board(50, 20, 1, 0.5);
-        Assert.Equal(expected.Height, result.Height);
-        Assert.Equal(expected.CellSize, result.CellSize);
-        Assert.Equal(expected.Columns, result.Columns);
-        Assert.Equal(expected.Rows, result.Rows);
-        Assert.Equal(expected.Width, result.Width);
-    }
-    [Fact]
-    public void Test2()
-    {
-        Program.Reset();
-        Program.load("out2.txt");
-        Assert.Equal(Program.find("block"),2);
-    }
-    [Fact]
-    public void Test3()
-    {
-        Program.Reset();
-        Program.load("out2.txt");
-        Assert.Equal(Program.find("box"),1);
-    }
-    [Fact]
-    public void Test4()
-    {
-        Program.Reset();
-        Program.load("out2.txt");
-        Assert.Equal(Program.symmetry_horizontally(),false);
-    }
-    [Fact]
-    public void Test5()
-    {
-        Program.Reset();
-        Program.load("out2.txt");
-        Assert.Equal(Program.symmetry_vertically(),false);
+        [TestMethod]
+        public void Test1()
+        {
+            Program.Reset();
+            Board result = Program.board;
+            Board expected = new Board(50, 20, 1, 0.5);
+            Assert.AreEqual(expected.Height, result.Height);
+            Assert.AreEqual(expected.CellSize, result.CellSize);
+            Assert.AreEqual(expected.Columns, result.Columns);
+            Assert.AreEqual(expected.Rows, result.Rows);
+            Assert.AreEqual(expected.Width, result.Width);
+        }
+        [TestMethod]
+        public void Test2()
+        {
+            Program.Reset();
+            Program.load("out2.txt");
+            Assert.AreEqual(Program.find("block"),2);
+        }
+        [TestMethod]
+        public void Test3()
+        {
+            Program.Reset();
+            Program.load("out2.txt");
+            Assert.AreEqual(Program.find("box"),1);
+        }
+        [TestMethod]
+        public void Test4()
+        {
+            Program.Reset();
+            Program.load("out2.txt");
+            Assert.AreEqual(Program.symmetry_horizontally(),false);
+        }
+        [TestMethod]
+        public void Test5()
+        {
+            Program.Reset();
+            Program.load("out2.txt");
+            Assert.AreEqual(Program.symmetry_vertically(),false);
+        }
     }
 }
