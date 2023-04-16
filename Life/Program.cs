3340d197e7d74f8bb4e7454bea1dede53eaede07
den9995 <den9995@gmail.com>
2023-04-16 20:31:32 +0300

    base functionality

--- Life/Program.cs
+++ Life/Program.cs
@@ -2,11 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using System.Threading;
+using System.IO;
 
 namespace cli_life
 {
+    public class Figure
+    {
+        public int m { get; set; }
+        public int n { get; set; }
+        public bool[,] table { get; set; } 
+    }
+    public class Config
+    {
+        public int width { get; set; }
+        public int height { get; set; }
+        public int cellSize { get; set; }
+        public double density { get; set; }
+    }
+
     public class Cell
     {
         public bool IsAlive;
@@ -29,11 +45,13 @@ namespace cli_life
     {
         public readonly Cell[,] Cells;
         public readonly int CellSize;
+        public double liveDensity;
 
         public int Columns { get { return Cells.GetLength(0); } }
         public int Rows { get { return Cells.GetLength(1); } }
         public int Width { get { return Columns * CellSize; } }
         public int Height { get { return Rows * CellSize; } }
+        public double LiveDensity { get { return liveDensity; } }
 
         public Board(int width, int height, int cellSize, double liveDensity = .1)
         {
@@ -91,11 +109,13 @@ namespace cli_life
         static Board board;
         static private void Reset()
         {
-            board = new Board(
-                width: 50,
-                height: 20,
-                cellSize: 1,
-                liveDensity: 0.5);
+            string json = File.ReadAllText(@"./config.json");
+            Config data = JsonSerializer.Deserialize<Config>(json);
+            int width = data.width;
+            int height = data.height;
+            int cellSize = data.cellSize;
+            double density = data.density;
+            board = new Board(width: width, height: height, cellSize: cellSize,liveDensity: density);
         }
         static void Render()
         {
@@ -119,13 +139,154 @@ namespace cli_life
         static void Main(string[] args)
         {
             Reset();
+            if(new FileInfo("out.txt").Exists)
+                load("out.txt");
             while(true)
             {
                 Console.Clear();
                 Render();
+                save();
+                Console.WriteLine("blocks:"+find("block"));
+                Console.WriteLine("boxes:"+find("box"));
+                Console.WriteLine("horizontal symetry:"+symmetry_horizontally());
+                Console.WriteLine("vertical symetry:"+symmetry_vertically());
                 board.Advance();
-                Thread.Sleep(1000);
+                Thread.Sleep(10);
             }
+        }
+        public static void save()
+        {
+            string fileName = "out.txt";
+            string str = "";
+            StreamWriter f = new StreamWriter(fileName, false);
+            f.WriteLine(board.Columns.ToString());
+            f.WriteLine(board.Rows.ToString());
+            for (int row = 0; row < board.Rows; row++)
+            {
+                str = "";
+                for (int col = 0; col < board.Columns; col++)
+                {
+                    if (board.Cells[col, row].IsAlive == true)
+                    {
+                        str = str + "*";
+                    }
+                    else
+                    {
+                        str = str + "_";
+                    }
+                }
+                f.WriteLine(str);
+            }
+            f.Close();
+        }
+        public static void load(string str)
+        {
+            StreamReader f = new StreamReader(str);
+            int width = Int32.Parse(f.ReadLine());
+            int height = Int32.Parse(f.ReadLine());
+            board = new Board(width: width, height: height, cellSize: 1, liveDensity: 0.5 );
+            int j = 0;
+                while (!f.EndOfStream)
+                {
+                    string s = f.ReadLine();
+                    for (int i = 0; i < width; i++)
+                    {
+                        if (s[i] == '*')
+                        board.Cells[i,j].IsAlive = true;
+                        else
+                        board.Cells[i, j].IsAlive = false;
+                    }
+                    j++;
+                }
+            f.Close();
+        }
+        public static bool symmetry_horizontally()
+        {
+
+            bool flag = true;
+            for (int i=0; i<board.Columns; i++)
+            {
+                for (int j = 0; j < board.Rows/2; j++)
+                {
+                    if (board.Cells[i, j].IsAlive != board.Cells[i,board.Rows-1-j].IsAlive)
+                        flag = false;   
+                }
+            }
+            return flag;
+        }
+        public static bool symmetry_vertically()
+        {
+            bool flag = true;
+            for (int i = 0; i < board.Rows; i++)
+            {
+                for (int j = 0; j < board.Columns / 2; j++)
+                {
+                    if (board.Cells[j, i].IsAlive != board.Cells[board.Columns - 1 -j,  i].IsAlive)
+                        flag = false;
+                }
+            }
+            return flag;
+        }
+        public static int find(string name)
+        {
+            StreamReader f = new StreamReader(name+".txt");
+            int width = Int32.Parse(f.ReadLine());
+            int height = Int32.Parse(f.ReadLine());
+            bool [,] table = new bool[width,height];
+            int j = 0;
+            while (!f.EndOfStream)
+            {
+                string s = f.ReadLine();
+                for (int i = 0; i < width; i++)
+                {
+                    if (s[i] == '*')
+                    table[i,j] = true;
+                    else
+                    table[i,j] = false;
+                }
+                j++;
+            }
+            f.Close();
+            
+            j = 0;
+            int rowIndexStart = 0;
+            int rowIndexFinish = width;
+            int colIndexStart = 0;
+            int colIndexFinish = height;
+            bool flag = true;
+            int sum = 0;
+            while (rowIndexFinish < board.Columns)
+            {
+                while (colIndexFinish < board.Rows)
+                {
+                    int i = 0;
+                    j = 0;
+                    for (int row = rowIndexStart; row < rowIndexFinish; row++)
+                    {
+                        for (int col = colIndexStart; col < colIndexFinish; col++)
+                        {
+                            if (board.Cells[row, col].IsAlive != table[i,j])
+                            {
+                                flag = false;
+                                break;
+                            }
+                            j++;
+                        }
+                        j = 0;
+                        i++;
+                    }
+                    i = 0;
+                    colIndexStart++;
+                    colIndexFinish++;
+                    if (flag) sum++;
+                    flag = true;
+                }
+                colIndexFinish = height;
+                colIndexStart = 0;
+                rowIndexStart++;
+                rowIndexFinish++;
+            }
+            return sum;
         }
     }
 }
